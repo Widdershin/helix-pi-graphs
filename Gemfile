52d920eb209b5e740115f025fb3a0d766d9d98ed
Nick Johnstone <ncwjohnstone@gmail.com>
2015-08-09 19:58:10 +1200

    Add a build status thingy and a header

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor'
 
 gem 'chartkick'
+
+gem 'normalize-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -46,5 +48,8 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'pry-rails'
+  gem 'pry-byebug'
 end
 
---
+++ app/presenters/build_status_presenter.rb
@@ -0,0 +1,52 @@
+class BuildStatusPresenter < Struct.new(:bench)
+  delegate :created_at, :success?, :to => :bench
+
+  def to_html
+    return ht 'div', 'Push to run benchmarks' if bench.nil?
+
+    ht 'div' do |ha|
+      ha.('span', 'Last Benchmark: ')
+      ha.('span', "#{small_sha} - #{created_at} ago", class: "build #{benchmark_status}")
+    end
+  end
+
+  private
+
+  def small_sha
+    bench.commit.sha.slice(0, 6)
+  end
+
+  def created_at
+    date_helper.time_ago_in_words(bench.created_at)
+  end
+
+  def benchmark_status
+    if success?
+      'passing'
+    else
+      'failing'
+    end
+  end
+
+  def ht(*args)
+    return html_helper.content_tag(*args) unless block_given?
+
+    html_helper.content_tag(*args) do
+      children = []
+
+      yield(-> (*innerArgs, &block) {
+        children << ht(*innerArgs, &block)
+      })
+
+      children.sum
+    end
+  end
+
+  def html_helper
+    @html_helper ||= ActionView::Base.new.extend(ActionView::Helpers::TagHelper)
+  end
+
+  def date_helper
+    @date_helper ||= ActionView::Base.new.extend(ActionView::Helpers::DateHelper)
+  end
+end
